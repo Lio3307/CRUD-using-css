a7be5f4df1ffb1616816f9f34972402edc9c7a61
Lio3307 <143271305+Lio3307@users.noreply.github.com>
2023-11-23 21:24:48 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,21 +1,3 @@
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($username === 'admin' && $password === 'admin123') {
-        // Jika login berhasil akan di arahkan ke halaman admin
-        $_SESSION['loggedin'] = true;
-        header("Location: admin.php");
-        exit;
-    } else {
-        // Jika login gagal maka akan eror 
-        echo "Username atau password salah!";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -27,14 +9,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 <div class="container">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <form action="admin.php" method="post">
     <h2>Login</h2>
         <label for="username">Username:</label>
         <input type="text" name="username" required><br><br>
         <label for="password">Password:</label>
         <input type="password" name="password" required><br><br>
-        <input type="submit" value="Login">
+        <input type="submit" name="login" value="Login">
     </form>
     </div>
+    <?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === 'admin' && $password === 'admin123') {
+        // Jika login berhasil akan di arahkan ke halaman admin
+        $_SESSION['login'] = true;
+        header("Location: admin.php");
+        exit;
+    } else {
+        // Jika login gagal maka akan eror 
+        echo "Username atau password salah!";
+    }
+}
+?>
+
 </body>
 </html>
