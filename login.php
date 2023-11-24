57293cd354aad36af0515a0572eaea2fd440a3da
Lio3307 <143271305+Lio3307@users.noreply.github.com>
2023-11-24 15:31:27 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -9,13 +9,32 @@
 </head>
 <body>
 <div class="container">
-    <form action="admin.php" method="post">
+    <form action="login.php" method="post">
     <h2>Login</h2>
         <label for="username">Username:</label>
         <input type="text" name="username" required><br><br>
         <label for="password">Password:</label>
         <input type="password" name="password" required><br><br>
         <input type="submit" name="login" value="Login">
+
+        <?php
+        session_start();
+        $username = "admin";
+        $password = "admin";
+
+        if ( !isset($_POST['username'], $_POST['password']) ) {
+        	// Could not get the data that should have been sent.
+        	exit('<p>Please fill both the username and password fields!</p>');
+        }
+
+        if ($_POST['password'] === $password){
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            header('Location: admin.php');
+        }else{
+            echo "<script>alert ('incorrect username or password')</script>";
+        }
+        ?>
     </form>
     </div>
 </body>
